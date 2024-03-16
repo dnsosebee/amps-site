e67f36cd6eefc2cb5ee24ffb3ad3c53ecaa81dd7
Daniel Sosebee <dnsosebee@gmail.com>
2024-03-15 18:33:21 -0700

    better prose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <main className="mx-auto max-w-7xl sm:px-6 lg:px-8 sm:py-6 lg:py-8 prose">
-        <h1>Agent Meta-Programming Systems Runtime</h1>
+        <h1>Agent Meta-Programming System Runtime</h1>
         <p>
           AMPS is a research project to develop a language and runtime for meta-self-improving AI
           agents. Unlike existing AI agents, AMPS agents will live in a{" "}
@@ -16,8 +16,8 @@ export default function Home() {
         <h3>Agenda</h3>
         <ol>
           <li>
-            Initial research - review of existing literature and technology to identify gaps and
-            opportunities.
+            Initial research - review existing literature on AI agents, planning, and executive
+            functioning.
           </li>
           <li>
             Problem identification - collect problems that will serve as a realistic test of the
@@ -28,23 +28,24 @@ export default function Home() {
             out loud, on video and in screen recording.
           </li>
           <li>
-            Pseudocode development - develop pseudocode based on human recordings to describe
-            symbolically the processing done by the human.
+            Pseudocode development - Write pseudocode programs based on human trial data to
+            symbolically describe the human perceptions, thoughts, and actions.
           </li>
           <li>
-            Language spec development - based on that pseudocode develop a language spec that will
-            be used to implement the AMPS runtime.
+            Language spec development - Inspired by the human pseudocode, write a simple language
+            spec for the AMPS language.
           </li>
           <li>
-            Initial Python library development - develop a Python library that will be used to
-            implement the AMPS runtime.
+            Initial Python library development - develop a Python library that implements the AMPS
+            language.
           </li>
           <li>
-            Iterative testing of language - try out the runtime on the problem list and iteratively
-            update the language.
+            Iterative system improvement - execute test runs against the problem list and record
+            success metrics. Use the results to iteratively improve the system and reach a v1
+            language spec.
           </li>
           <li>
-            Fine-tuning data collection - collect a dataset of AMPS frames and correct outputs.
+            Fine-tuning data collection - collect a dataset of AMPS contexts and correct outputs.
           </li>
           <li>
             LLM fine-tuning - fine-tune an open-source language model on the dataset in order to
